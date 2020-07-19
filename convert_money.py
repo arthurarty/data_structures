3cd4088d0ebe3dd0e0754708f325d45a8776a0a2
arthurarty <arthur.nangai@gmail.com>
2020-07-19 16:03:33 +0300

    add edge cases.

--- convert_money.py
+++ convert_money.py
@@ -3,11 +3,18 @@ def convert_money(money:str) -> int:
         return money
     money = money.strip('$')
     money = money.lower()
+    multiplier = 1
     for char in money:
         if char == 'm':
             money = money.strip('m')
-            money = float(money) * 1000000
-    return int(money)
+            multiplier = 1000000
+        if char == 'k':
+            money = money.strip('k')
+            multiplier = 1000
+    try:
+        money = float(money) * multiplier
+    except ValueError:
+        return
 
 
 print(convert_money('$14.2M'))
