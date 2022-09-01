310e41a73fe03b4cf7b824100fe841022363f320
Arthur Nangai <arthur.nangai@gmail.com>
2022-09-01 11:06:55 +0300

    minor change

--- binary_search.py
+++ binary_search.py
@@ -22,4 +22,4 @@ def binary_search(input_list: List[int], val: int) -> Optional[int]:
 the_list = [2, 7, 19, 34, 53, 72]
 print(binary_search(the_list, 72))
 print(binary_search(the_list, 78))
-print(binary_search([72, 34, 7, 2, 29]))
+print(binary_search([72, 34, 7, 2, 29, 19], 72))
