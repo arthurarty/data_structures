dc2d9cf7791b26fdbc34cf57fff5cbde7960a140
Arthur Nangai <arthur.nangai@gmail.com>
2022-09-01 11:06:04 +0300

    sort list before search

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 
 def binary_search(input_list: List[int], val: int) -> Optional[int]:
-
+    input_list.sort()  # we cannot assume the list will be sorted.
     low = 0
     high = len(input_list) - 1
 
@@ -22,3 +22,4 @@ def binary_search(input_list: List[int], val: int) -> Optional[int]:
 the_list = [2, 7, 19, 34, 53, 72]
 print(binary_search(the_list, 72))
 print(binary_search(the_list, 78))
+print(binary_search([72, 34, 7, 2, 29]))
